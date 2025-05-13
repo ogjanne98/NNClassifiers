b6732aa1b0c1b5e51af184b74c44d7b9df8e59c8
ogjanne98 <156075817+ogjanne98@users.noreply.github.com>
2025-05-13 19:02:32 +0200

    Updated NNClassifier.py
    Implemented a few methods, added a few commments and some method documentation.

--- NNClassifier.py
+++ NNClassifier.py
@@ -1,34 +1,77 @@
+import numpy as np
 
 class NNClassifier:
     def __init__(self):
         pass
 
     def fit(self, X, y, X_val, y_val, hidden_layers=[1], batch_size="full", max_epochs=100, lr=0.1):
-        # initialize properties
-        self.n_features = X.shape[1]
-        self.n_classes = y.shape[1] #assume that targets are 1-hot
-        self.weights = self.initialize_weights() #initialize weights
-        self.batch_size = batch_size
+        """
+        Description:
+        TBC
 
-        stopping_condition = False # to be defined
+        Parameters:
+        TBC
+
+        Returns:
+        TBC
+        """
+        # Initialize properties
+        self.n_features = X.shape[1]
+        self.n_classes = y.shape[1] # Assume that targets are 1-hot
+        self.weights = self.initialize_weights([self.n_features] + hidden_layers + [self.n_classes]) # Array of weight matrices
+        self.batch_size = batch_size 
+        self.X_val = X_val
+        self.y_val = y_val
+        self.n_val_loss_increase = 0
+        self.val_loss = 0
+
+        # Main loop
+        self.stopping_condition = False 
         epoch = 0
-        while (not stopping_condition) and (epoch < max_epochs):
+        while (not self.stopping_condition) and (epoch < max_epochs):
             self.update_weights()
-            stopping_condition = self.update_condition()
+            self.update_condition()
             epoch += 1
 
 
     def predict(self):
-        # to be implemented    
+        pass # To be implemented    
 
-    def initialize_weigths(self):
-        # to be implemented
+    def initialize_weigths(self, layers):
+        """
+        Description:
+        Initialize the weight matrices in the neural network uniform random numbers in [-1,1].
+
+        Parameters:
+        layers (list): The number of nodes in each layer, from the input to the output.
+
+        Returns:
+        weights (list): All of the weights matrices, from the first to the last.
+        """
+        weights = []
+        for l1,l2 in zip(layers[:-1], layers[1:]):
+            weights.append(np.random.uniform(low=-1, high=1, size=(l1,l2)))
+        return weights    
 
     def update_condition(self):
-        # to be implemented    
+        """
+        Description:
+        Updates early stoppage condition in neural network. Stops if the validation loss increases over several epochs in a row.
+        Stops after strictly greater than 3 epochs.
+        """
+        ls = self.loss(self.X_val, self.y_val)
+        if  ls > self.val_loss:
+            self.n_val_loss_increase += 1
+        else:
+            self.n_val_loss_increase = 0
+        self.val_loss = ls  
+        self.stopping_condition = (self.n_val_loss_increase > 3)      
 
     def update_weights(self):
         raise NotImplementedError("Superclass not to be instantiated")  
     
     def forward(self):
+        raise NotImplementedError("Superclass not to be instantiated")
+    
+    def loss(self):
         raise NotImplementedError("Superclass not to be instantiated")
